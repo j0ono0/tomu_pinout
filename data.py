312d8b0f91abb6e9a8f10a14ec596eefd92bda05
j0ono0 <john@johnnewall.com>
2021-08-19 14:26:28 +0930

    creation of layout template

--- data.py
+++ data.py
@@ -1,71 +1,71 @@
-fomu_pvt = {
-    "title": "<tspan class='h1'>FOMU PVT (FPGA TOMU)</tspan>",
-    "description": """Features of the FOMU development board.
-    https://tomu.im/fomu.html
-    """,
-    "image": {
-        "front": "./images/tomu-fpga-dvt1c-front.png",
-        "back": "./images/tomu-fpga-dvt1c-back.png",
+title = "<tspan class='h1'>FOMU PVT (FPGA TOMU)</tspan>"
+description = """Features of the FOMU development board.
+    https://tomu.im/fomu.html"""
+
+
+fomu_annotations = [
+    {
+        "name": "RGB LED",
+        "coords": (864, 650),
+        "config": {"y": 12, "scale": (1, -1)},
     },
-    "annotations": [
-        {
-            "name": "RGB LED",
-            "coords": (864, 650),
-            "config": {"y": 12, "scale": (1, -1)},
-        },
-        {"name": "FPGA", "coords": (864, 1017), "config": {"y": 55, "scale": (-1, 1)}},
-        {
-            "name": "SPI Flash",
-            "coords": (285, 1308),
-            "config": {"y": 12, "scale": (-1, -1)},
-        },
-        {
-            "name": "Oscillator",
-            "coords": (1422, 1470),
-            "config": {"y": 20, "scale": (1, -1)},
-        },
-        {
-            "name": "v_reg_1",
-            "coords": (528, 2043),
-            "config": {"y": 40, "scale": (1, 1)},
-        },
-        {
-            "name": "v_reg_2",
-            "coords": (1260, 2043),
-            "config": {"y": 40, "scale": (1, 1)},
-        },
-        {
-            "name": "Voltage regulators",
-            "coords": (1545, 2043),
-            "config": {"y": 40, "scale": (1, 1)},
-        },
-    ],
-    "labelgroups": [
-        {
-            "name": "buttons",
-            "pin_start": (92, 278),
-            "label_start": (100, 0),
-            "pin_pitch": (0, 400),
-            "scale": (-1, 1),
-            "labels": [
-                [("btn 1", "btn")],
-                [("btn 2", "btn")],
-                [("btn 3", "btn")],
-                [("btn 4", "btn")],
-            ],
-        },
-        {
-            "name": "usb_pins",
-            "pin_start": (2090, 278),
-            "label_start": (100, 0),
-            "pin_pitch": (0, 400),
-            "scale": (1, 1),
-            "labels": [
-                [("Vcc", "pwr")],
-                [("Data [+]", "data")],
-                [("Data [-]", "data")],
-                [("GND", "gnd")],
-            ],
-        },
-    ],
-}
+    {
+        "name": "FPGA",
+        "coords": (864, 1017),
+        "config": {"y": 55, "scale": (-1, 1)},
+    },
+    {
+        "name": "SPI Flash",
+        "coords": (285, 1308),
+        "config": {"y": 12, "scale": (-1, -1)},
+    },
+    {
+        "name": "Oscillator",
+        "coords": (1422, 1470),
+        "config": {"y": 20, "scale": (1, -1)},
+    },
+    {
+        "name": "v_reg_1",
+        "coords": (528, 2043),
+        "config": {"y": 40, "scale": (1, 1)},
+    },
+    {
+        "name": "v_reg_2",
+        "coords": (1260, 2043),
+        "config": {"y": 40, "scale": (1, 1)},
+    },
+    {
+        "name": "Voltage regulators",
+        "coords": (1545, 2043),
+        "config": {"y": 40, "scale": (1, 1)},
+    },
+]
+
+fomu_pinlabels = [
+    {
+        "name": "buttons",
+        "pin_start": (92, 278),
+        "label_start": (100, 0),
+        "pin_pitch": (0, 400),
+        "scale": (-1, 1),
+        "labels": [
+            [("btn 1", "btn")],
+            [("btn 2", "btn")],
+            [("btn 3", "btn")],
+            [("btn 4", "btn")],
+        ],
+    },
+    {
+        "name": "usb_pins",
+        "pin_start": (2090, 278),
+        "label_start": (100, 0),
+        "pin_pitch": (0, 400),
+        "scale": (1, 1),
+        "labels": [
+            [("Vcc", "pwr")],
+            [("Data [+]", "data")],
+            [("Data [-]", "data")],
+            [("GND", "gnd")],
+        ],
+    },
+]
--- pinout_diagram.py
+++ pinout_diagram.py
@@ -14,130 +14,37 @@ from pinout.components.pinlabel import PinLabelGroup
 from pinout.components.annotation import AnnotationLabel
 from pinout import config
 
+# Experimental layout class!!!!!!!
+from template import LayoutA
+
 # Import data for the diagram
 # from data import fomu_pvt as data
-from data import fomu_pvt as data
+import data
 
 # Modify default component configs
 config.annotation["body"]["width"] = 160
 
+layout = LayoutA(1024, 800, 700, "diagram")
+diagram = layout.diagram
+layout.add_title(data.title)
+layout.add_description(data.description)
 
-# Create a new diagram
-diagram = Diagram(1024, 800, "diagram")
-
-# Add a stylesheet
-diagram.add_stylesheet("styles.css", embed=True)
-
-# Create a layout
-content = diagram.add(
-    Panel(
-        width=1024,
-        height=800,
-        inset=(2, 2, 2, 2),
-    )
+layout.add_graphic(
+    {
+        "x": 512,
+        "y": 200,
+        "image": "./images/tomu-fpga-dvt1c-back.png",
+        "annotations": data.fomu_annotations,
+    }
 )
-panel_main = content.add(
-    Panel(
-        width=content.inset_width,
-        height=700,
-        inset=(2, 2, 2, 2),
-        tag="panel--main",
-    )
+layout.add_graphic(
+    {
+        "x": 512,
+        "y": 560,
+        "image": "./images/tomu-fpga-dvt1c-front.png",
+        "pinlabels": data.fomu_pinlabels,
+    }
 )
-panel_info = content.add(
-    Panel(
-        x=0,
-        y=panel_main.height,
-        width=panel_main.width,
-        height=content.inset_height - panel_main.height,
-        inset=(2, 2, 2, 2),
-        tag="panel--info",
-    )
-)
-
-# Create a title and a text-block
-title_block = panel_info.add(
-    TextBlock(
-        data["title"],
-        x=20,
-        y=30,
-        line_height=18,
-        tag="panel title_block",
-    )
-)
-panel_info.add(
-    TextBlock(
-        data["description"],
-        x=20,
-        y=60,
-        width=title_block.width,
-        height=panel_info.height - title_block.height,
-        line_height=18,
-        tag="panel text_block",
-    )
-)
-
-# Create a group to hold the pinout-diagram components.
-graphic = panel_main.add(Group(512, 0))
-
-# Add and embed an image
-hardware_back = graphic.add(
-    Image(data["image"]["back"], x=-150, y=40, width=300, height=300, embed=True)
-)
-hardware_front = graphic.add(
-    Image(data["image"]["front"], x=-150, y=380, width=300, height=300, embed=True)
-)
-
-# Add actual pixel dimension to easily extract scaled dimensions
-hardware_back.add_coord("size", *hardware_back.im_size)
-
-
-#####################################################
-# Annotations
-if "annotations" in data.keys():
-    # x offset from edge of board
-    offset = 40
-    img_width = hardware_back.coord("size", raw=True).x
-    x_rhs = hardware_back.coord("size").x + offset
-
-    for anno in data["annotations"]:
-        # Add coords to Image instance
-        hardware_back.add_coord(anno["name"], *anno["coords"])
-        # Calculate x position of annotation body
-        if anno["config"]["scale"][0] == 1:
-            # label right of image
-            x = offset + img_width - hardware_back.coord(anno["name"], True).x
-        else:
-            # label left of image
-            x = offset + hardware_back.coord(anno["name"], True).x
-        # Create annotation label
-        graphic.add(
-            AnnotationLabel(
-                x=hardware_back.coord(anno["name"]).x,
-                y=hardware_back.coord(anno["name"]).y,
-                content=anno["name"],
-                body={"x": x, "y": anno["config"]["y"]},
-                scale=anno["config"]["scale"],
-            )
-        )
-
-#####################################################
-# Pinlabels
-if "labelgroups" in data.keys():
-    for grp in data["labelgroups"]:
-        hardware_front.add_coord("pin_start", *grp["pin_start"])
-        hardware_front.add_coord("pin_pitch", *grp["pin_pitch"])
-        graphic.add(
-            PinLabelGroup(
-                *hardware_front.coord("pin_start"),
-                pin_pitch=hardware_front.coord("pin_pitch", True),
-                label_start=grp["label_start"],
-                label_pitch=(0, 30),
-                labels=grp["labels"],
-                scale=grp["scale"],
-            )
-        )
-
 
 # Export the diagram via commandline:
 # >>> py -m pinout.manager -e pinout_diagram output/fomu_pinout.svg -o
